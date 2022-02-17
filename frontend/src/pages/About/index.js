322e6f1c773d0be8d65c5b40bce48336f0a579dc
Juliano-Formigari <julianomix@gmail.com>
2022-02-16 23:06:48 -0300

    Inicializando configurações do template

---
+++ frontend/src/pages/About/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = () => {
+    return(
+        <div>Initial Page about!!</div>
+    );
+}
+
+export default Page;
---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = () => {
+    return(
+        <div>Initial Page!!</div>
+    );
+}
+
+export default Page;
