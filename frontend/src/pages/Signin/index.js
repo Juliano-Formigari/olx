ff9ce27c07f084bbb28c43253d8a4a29dd0b61e7
Juliano-Formigari <julianomix@gmail.com>
2022-03-09 22:54:33 -0300

    Adicionando funcionalidade na página de login

--- frontend/src/pages/Signin/index.js
+++ frontend/src/pages/Signin/index.js
@@ -1,36 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { PageArea } from './styled';
+import useApi from '../../helpers/OlxApi';
+import { doLogin } from "../../helpers/AuthHandler";
 
-import { PageContainer, PageTitle } from '../../components/MainComponents';
+
+import { PageContainer, PageTitle, ErrorMessage } from '../../components/MainComponents';
 
 const Page = () => {
+    const api = useApi();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberPassword, setRememberPassword] = useState(false);
+    const [disable, setDisable] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setDisable(true);
+        
+        const json = await api.login(email, password);
+        
+        if (json.error) {
+            setError(json.error)
+        } else {
+            doLogin(json.token, rememberPassword);
+            window.location.href = '/';
+        }
+        setDisable(false);
+    }
+
     return (
         <PageContainer>
             <PageTitle>Login</PageTitle>
             <PageArea>
-                <form>
+                {error &&
+                    <ErrorMessage>{error}</ErrorMessage>
+                }
+
+
+                <form onSubmit={handleSubmit}>
                     <label className="area">
                         <div className="area--title">E-mail</div>
                         <div className="area--input">
-                            <input type="email"></input>
+                            <input
+                                type="email"
+                                disabled={disable}
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                required
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Senha</div>
                         <div className="area--input">
-                            <input type="password"></input>
+                            <input
+                                type="password"
+                                disabled={disable}
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                required
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Lembrar Senha</div>
                         <div className="area--input">
-                            <input type="checkbox"></input>
+                            <input
+                                type="checkbox"
+                                disabled={disable}
+                                checked={rememberPassword}
+                                onChange={() => setRememberPassword(!rememberPassword)}
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title"></div>
                         <div className="area--input">
-                            <button>Fazer Login</button>
+                            <button disabled={disable}>Fazer Login</button>
                         </div>
                     </label>
                 </form>
